4e79759f1328279a9bccf8816ec75a9847ab6c7c
Annie Bu <annie.bu@mail.utoronto.ca>
2024-10-28 17:51:45 -0400

    all

--- src/main/java/interface_adapter/change_password/LoggedInViewModel.java
+++ src/main/java/interface_adapter/change_password/LoggedInViewModel.java
@@ -1,11 +1,14 @@
 package interface_adapter.change_password;
 
 import interface_adapter.ViewModel;
+import interface_adapter.logout.LogoutController;
 
 /**
  * The View Model for the Logged In View.
  */
 public class LoggedInViewModel extends ViewModel<LoggedInState> {
+
+    private LogoutController logoutController;
 
     public LoggedInViewModel() {
         super("logged in");
--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -4,9 +4,15 @@ package use_case.logout;
  * The Input Data for the Logout Use Case.
  */
 public class LogoutInputData {
+    String name;
 
     public LogoutInputData(String username) {
         // TODO: save the current username in an instance variable and add a getter.
+        name = username;
+    }
+
+    public String getUsername() {
+        return name;
     }
 
 }
